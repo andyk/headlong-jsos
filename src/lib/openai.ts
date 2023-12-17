f6d9c3e1d17fd71d904febbc8c7f77e2d5f2856a
Andy Konwinski <andykonwinski@gmail.com>
2023-12-16 16:52:13 -0800

    test

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -1,7 +1,7 @@
 
 import OpenAI from "openai";
 const openAiOrg = import.meta.env.OPENAI_ORG;
-const openAiApiKey = import.meta.env.OPENAI_API_KEY;
+const openAiApiKey = import.meta.env.OPENAI_API_KEY || VITE_VERCEL_ENV;
 console.log("openAiApiKey: ", openAiApiKey);
 const openai = new OpenAI({
     organization: openAiOrg,
