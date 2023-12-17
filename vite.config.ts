1842887bbd46e3c815099304197bbfe5ab0452a4
Andy Konwinski <andykonwinski@gmail.com>
2023-12-16 16:56:33 -0800

    trying to get openai Env to work

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,9 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: { exclude: ["fsevents"] },
   define: {
-    'import.meta.env.SUPABASE_URL_JSOS': process.env.SUPABASE_URL_JSOS,
-    'import.meta.env.SUPABASE_SERVICE_ROLE_KEY_JSOS': process.env.SUPABASE_SERVICE_ROLE_KEY_JSOS,
-    'import.meta.env.OPENAI_API_KEY': process.env.OPENAI_API_KEY,
-    'VITE_VERCEL_ENV': process.env.VITE_VERCEL_ENV,
+    'import.meta.env.SUPABASE_URL_JSOS': JSON.stringify(process.env.SUPABASE_URL_JSOS),
+    'import.meta.env.SUPABASE_SERVICE_ROLE_KEY_JSOS': JSON.stringify(process.env.SUPABASE_SERVICE_ROLE_KEY_JSOS),
+    'import.meta.env.OPENAI_API_KEY': JSON.stringify(process.env.OPENAI_API_KEY),
+    'VITE_VERCEL_ENV': JSON.stringify(process.env.VITE_VERCEL_ENV)
   }
 })
